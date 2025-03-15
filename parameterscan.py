7cdb5aa5f3a791f9f9228f2a22f582e1f9f25720
Liam1337V <167616041+Liam1337V@users.noreply.github.com>
2025-03-15 16:04:02 +0100

    Update parameterscan.py

--- parameterscan.py
+++ parameterscan.py
@@ -13,13 +13,13 @@ os.chdir(repertoire)
 input_filename = 'configuration.in.example'  # Name of the input file
 
 
-nsteps = np.array([10, 20 , 50 , 100 , 150 , 200]) # TODO change
+nsteps = np.array([50 , 100 , 150 , 200 , 300]) # TODO change
 #nsteps = np.array([10])
 nsimul = len(nsteps)  # Number of simulations to perform
 
 tfin = 7776000  # Done : Verify that the value of tfin is EXACTLY the same as in the input file
 
-dt = tfin / nsteps
+#dt = tfin / nsteps
 
 energy = np.zeros(nsimul) # added 
 
@@ -60,6 +60,21 @@ Nperiod = Values[11,-1]
 
 w0 = np.sqrt( 12 * mu * B0 / ( m * L ** 2 ) )
 
+def calcul_dt ( Excitation = True ) : # calcule le pas temporel ( retourne un array )
+
+    if Excitation :
+
+        Omega = 2 * w0
+        T = 2*np.pi / Omega 
+        dt = T / nsteps
+    
+    else :
+
+        T = 2*np.pi / w0
+        dt = T / nsteps
+
+    return dt 
+
 # ------------------------------ Valeurs Finales Pour Simulations -------------------------------#
 
 for i in range(nsimul):  # Iterate through the results of all simulations
@@ -87,45 +102,51 @@ def Emec () : # Affiche l'Emec en fonction du temps
     fig, ax = plt.subplots(constrained_layout=True)
     plt.ticklabel_format(axis='y', style='scientific', scilimits = (-3,-3))
     ax.plot(t, data[:,3], color = 'orange' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}")
-    ax.plot(t, np.mean(data[:,3])*np.ones(t.size) , color = "grey" , linestyle = "dashed" , label = '$<E_{mec}> = $' + f"{np.mean(data[:,3]):.4e}" ) 
+    #ax.plot(t, np.mean(data[:,3])*np.ones(t.size) , color = "grey" , linestyle = "dashed" , label = '$<E_{mec}> = $' + f"{np.mean(data[:,3]):.4e}" ) 
     ax.set_ylabel('$E_{mec}$', fontsize=fs)
-    ax.set_xlabel('$\\Delta t$ [s]', fontsize=fs)
+    ax.set_xlabel('$t$ [s]', fontsize=fs)
     plt.legend()
 
-def Pnc () : # Affiche la puissance des forces non-conservatives en fonction du temps (ajouter la dérivée de Emec après) 
+def Pnc () : # Affiche la puissance des forces non-conservatives en fonction du temps (ajouter la dérivée de Emec après)
+
+    dt = calcul_dt()[-1] 
+    
+    dtEmec = ( data[1:,3] - data[:-1,3] ) / dt
 
     fig, ax = plt.subplots(constrained_layout=True)
-    
-    plt.ticklabel_format(axis='y', style='scientific', scilimits = (-3,-3))
-    ax.plot(t, data[:,4], color = 'orange' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}")
+    #plt.ticklabel_format(axis='y', style='scientific', scilimits = (-3,-3))
+    ax.plot(t[:-1],dtEmec)
+    ax.plot(t, data[:,4], color = 'orange' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}" , linestyle = 'dashed')
     ax.set_ylabel('$P_{nc}$', fontsize=fs)
-    ax.set_xlabel('$\\Delta t$ [s]', fontsize=fs)
+    ax.set_xlabel('$t$ [s]', fontsize=fs)
     plt.legend() 
 
 def Theta () : # Affiche la position en fonction du temps 
     
     fig, ax = plt.subplots(constrained_layout=True)
-    ax.plot(t, 1e-06 * np.cos(w0 * t) , color = 'red' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}")    
-    ax.plot(t, data[:,1], color = 'black' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}", linestyle = "dashed")
+    #ax.plot(t, 1e-06 * np.cos(w0 * t) , color = 'red' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}")    
+    ax.plot(t, data[:,1], color = 'black' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}")
     ax.set_ylabel('$\\theta$', fontsize=fs)
-    ax.set_xlabel('$\\Delta t$ [s]', fontsize=fs)
+    ax.set_xlabel('$t$ [s]', fontsize=fs)
     plt.legend()
 
 def Thetadot () : # Affiche la vitesse en fonction du temps 
 
     fig, ax = plt.subplots(constrained_layout=True)
-    ax.plot(t, - theta0 * np.sin(w0 * t) * w0, color = 'red' , label = 'Analytique : $n_{step} = $' + f"{nsteps[-1]:.0f}")
+    #ax.plot(t, - theta0 * np.sin(w0 * t) * w0, color = 'red' , label = 'Analytique : $n_{step} = $' + f"{nsteps[-1]:.0f}")
     ax.plot(t, data[:,2], color = 'black' , label = '$n_{step} = $' + f"{nsteps[-1]:.0f}", linestyle = "dashed")
     ax.set_ylabel('$\\dot{\\theta}$', fontsize=fs)
-    ax.set_xlabel('$\\Delta t$ [s]', fontsize=fs)
+    ax.set_xlabel('$t$ [s]', fontsize=fs)
     plt.legend()
 
 
-def Delta (n_order = 2) : # Affiche l'erreur en fonction du nombre de pas 
+def Delta (n_order = 1) : # Affiche l'erreur en fonction du nombre de pas
+
+    dt = calcul_dt() 
 
     plt.figure()
-    plt.loglog(1/nsteps[:-1], delta[:-1], 'r+-', linewidth=lw)
-    plt.loglog(1/nsteps[:-1], theta0*pow(1/nsteps[:-1], n_order), color = 'black' ,linewidth = lw , label = f"$1/N^{n_order}$" , linestyle = 'dashed')
+    plt.loglog(dt, delta[:-1], 'r+-', linewidth=lw)
+    plt.loglog(dt, theta0*pow(1/nsteps[:-1], n_order), color = 'black' ,linewidth = lw , label = f"$1/N^{n_order}$" , linestyle = 'dashed')
     plt.xlabel('$\\Delta t$', fontsize=fs)
     plt.ylabel('$\\delta$', fontsize=fs)
     plt.xticks(fontsize=fs)
@@ -141,6 +162,18 @@ def Phase () :
     ax.set_xlabel('$\\theta$', fontsize=fs)
     plt.legend()
 
+def Theta_Convergeance (n_order = 2) :
+
+    dt = calcul_dt()
+
+    plt.figure()
+    plt.plot(dt**n_order, convergence_list, 'k+-', linewidth=lw)
+    plt.xlabel(f"$(\\Delta t)^{n_order}$ [s]", fontsize=fs)
+    plt.ylabel('$\\theta_{final}$', fontsize=fs)
+    plt.xticks(fontsize=fs)
+    plt.yticks(fontsize=fs)
+    plt.grid(True)
+
 def PointCarre ( cond_init ) :
 
     for i in cond_init :
@@ -152,18 +185,17 @@ def PointCarre ( cond_init ) :
         plt.plot(thetas,thetasdot)
 
     plt.show()
-    
-    print("A faire")
 
 
 # ------------------ Affichage ------------------ # 
 
-#Emec ()
-#Pnc ()
-Delta ()
-#Theta () 
+Emec ()
+Pnc ()
+#Delta ()
+Theta () 
 #Thetadot()
 Phase()
-#PointCarre () 
+#PointCarre ()
+Theta_Convergeance ()
 
 plt.show()
